67501a118c12dcc7e3f1df5bdfe30bfe7d966dd7
Dark-in-Star <sounakguha.12345@gmail.com>
2026-08-16 17:46:16 +0530

    Add creator credit to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ export function Footer() {
       <p>
         MyAniList is an unofficial client built on top of the MyAnimeList API. Anime and manga data © MyAnimeList.
       </p>
+      <p className="mt-1">Created by Dark-in-Star</p>
     </footer>
   );
 }
